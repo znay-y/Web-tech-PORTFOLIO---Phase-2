a3afe6bfaf07716bd1b764818ba7799b62e85a58
zaine <zaineirshad.07@gmail.com>
2026-05-06 11:33:49 +0100

    new file for showing preview posts

---
+++ viewPreview.php
@@ -0,0 +1,147 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/reset.css">
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="css/viewBlog.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Vast+Shadow&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Chivo+Mono:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Unica+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Alfa+Slab+One&display=swap" rel="stylesheet">
+
+    <title>Blog Posts</title>
+</head>
+
+<body>
+    <div id="main">
+        <nav>
+            <ul class="navlist">
+                <li id="name">
+                    <a href="viewBlog.php">Blog Posts</a>
+                </li>
+                <li id="home">
+                    <a href="index.php">Home</a>
+                </li>
+                <li id="edu">
+                    <a href="education.php">Education</a>
+                </li>
+                <li id="portfol">
+                    <a href="portfol.php">Portfolio</a>
+                </li>
+                <li id="skill">
+                    <a href="skills.php">Skills</a>
+                </li>
+                <li>
+                    <a href="viewBlog.php">Blog Posts</a>
+                </li>
+                <li id="logout">
+                    <a href="logout.php"><?php echo $_SESSION['fullname']; ?></a>
+                </li>
+            </ul>
+        </nav>
+
+        <section>
+            <aside>
+                <?php
+
+                $title = $_POST['Title'];
+                $body = $_POST['body'];
+
+
+                $servername = "127.0.0.1";
+                $username = "root";
+                $password = "";
+                $dbname = "ecs417";
+
+                $conn = new mysqli($servername, $username, $password, $dbname);
+
+                if ($conn->connect_error) {
+                    die("Connection failed: " . $conn->connect_error);
+                }
+
+                $sql = "SELECT * FROM posts";
+                $result = $conn->query($sql);
+
+                $posts = [];
+
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        $posts[] = $row;
+                    }
+                } else {
+                    echo "<h5>No posts found.</h5>";
+                }
+
+                for ($i = 0; $i < count($posts); $i++) {
+                    for ($j = 0; $j < count($posts) - 1; $j++) {
+                        if ($posts[$j]['id'] < $posts[$j + 1]['id']) {
+                            $temp = $posts[$j];
+                            $posts[$j] = $posts[$j + 1];
+                            $posts[$j + 1] = $temp;
+                        }
+                    }
+                }
+
+                
+
+
+
+                //Exsisting posts
+                foreach ($posts as $post) {
+                    echo "<div class='blog'>";
+                    echo "<h6>" . $post['created_at'] . "</h6>";
+                    echo "<h2>" . $post['title'] . "</h2>";
+                    echo "<hr>";
+                    echo "<p>" . $post['body'] . "</p>";
+                    echo "<hr>";
+                    echo "</div>";
+                }
+
+                $conn->close();
+
+                ?>
+                <a href
+                <a href="addEntry.php"><button>Confirm Changes</button></a>
+            </aside>
+
+        </section>
+
+        <footer>
+
+            <nav>
+                <ul id="bottomnav">
+                    <li>
+                        <a href="index.php">Home</a>
+                    </li>
+                    <li>
+                        <a href="education.php">Education</a>
+                    </li>
+                    <li>
+                        <a href="portfol.php">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="skills.php">Skills</a>
+                    </li>
+                    <li>
+                        <a href="viewBlog.php">Blog Posts</a>
+                    </li>
+                    <li>
+                        <a href="logout.php">Logout</a>
+                    </li>
+                </ul>
+            </nav>
+        </footer>
+    </div>
+</body>
+
+</html>
